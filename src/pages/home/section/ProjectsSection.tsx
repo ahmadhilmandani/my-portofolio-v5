ccc1bf1e951e6cad4f168d50fd82b196cd1a4297
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2025-07-29 09:07:56 +0700

    update: gsap on home projects section

--- src/pages/home/section/ProjectsSection.tsx
+++ src/pages/home/section/ProjectsSection.tsx
@@ -1,28 +1,51 @@
 import Button from "../../../components/Button.tsx";
 import Card from "../../../components/Card";
 import { data } from "../../../data.ts"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap"
 
 export default function ProjectsSection() {
+  // const gsapTimeline = gsap.timeline()
+  gsap.registerPlugin(ScrollTrigger)
+
+  useGSAP(() => {
+    gsap.fromTo('.project-content',
+      {
+        opacity: 0,
+        y: 10,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        scrollTrigger: {
+          trigger: '.project-content',
+          start: "bottom 60%",
+        },
+        stagger: 0.3,
+      }
+    )
+  }, [])
 
   return (
     <>
       <div className='w-full px-32 pt-32 pb-32'>
         <div className="mb-12">
           <div className="flex gap-8 items-stretch mb-5">
-            <div className="w-2 bg-gradient-to-b from-sage-500 to-sage-800">
+            <div className="project-content w-2 bg-gradient-to-b from-sage-500 to-sage-800">
 
             </div>
-            <h2>
+            <h2 className="project-content">
               Projects
             </h2>
           </div>
-          <p>Heare are some of my personal projects that i’ve been deployed. By all means, chek it out!</p>
+          <p className="project-content">Heare are some of my personal projects that i’ve been deployed. By all means, chek it out!</p>
         </div>
         <div className="flex gap-24 flex-wrap items-center justify-center mb-12">
           {data?.map((val, index) => {
-            if (index <= 2) {              
+            if (index <= 2) {
               return (
-                <>
+                <div className="project-content">
                   <Card
                     logo={val.logo}
                     projectType={val.type}
@@ -32,15 +55,14 @@ export default function ProjectsSection() {
                     projectId={val.id}
                     isPrimaryButtonToSite={val.isPrimaryButtonToSite}
                     techUsed={val.techUsed}
-  
                   />
-                </>
+                </div>
               )
             }
           })}
         </div>
-        <div className="flex justify-center">
-          <Button buttonType={'primary'}> 
+        <div className="project-content flex justify-center">
+          <Button buttonType={'primary'}>
             See More
           </Button>
         </div>
